1fbce6c0faa9924553b61579126d36afa17adc1d
a-kitaev <a.kitaev@outlook.com>
2019-02-07 15:48:11 -0500

    this and that

--- DHCFFooter/src/extensions/footer/components/IDHCFfooterProps.ts
+++
@@ -1,5 +0,0 @@
-import { ApplicationCustomizerContext } from '@microsoft/sp-application-base';
-
-export interface IDHCFfooterProps {
-    context: ApplicationCustomizerContext;
-}
--- DHCFFooter/src/extensions/footer/components/IDHCFfooterState.ts
+++
@@ -1,7 +0,0 @@
-export interface IDHCFfooterState {
-    editMode: boolean;
-    showPanel: boolean;
-    submited_data: string;
-    rating: number;
-    submited: boolean;
-}
